7b0c91cf55b9cfd74cd35707b5424d762585e1a8
Lucas Tavares <38914522+Tavaresl95@users.noreply.github.com>
2019-11-28 18:04:58 -0300

    Add files via upload
    Criando webpage

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#cadastrobotao").click(function(){
+    $('#login').hide()
+    $("#cadastro").fadeIn()
+    
+})
+
+$("#loginbotao").click(function(){
+    $("#cadastro").hide()
+    $('#login').fadeIn()
+})
+
